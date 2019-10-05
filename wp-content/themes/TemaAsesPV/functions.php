3ca6a5e9e020da422851d452b6aa9baa330013e8
errob <errobimd@gmail.com>
2019-10-06 00:50:28 +0200

    Definimos los styles con una funcion de wordpress (wp_enqueue_style) wp_enqueue_style( string $handle, string $src = '', array $deps = array(), string|bool|null $ver = false, string $media = 'all' ) Parámetros #Parámetros $ manejar ( cadena ) (obligatorio) Nombre de la hoja de estilo. Debería ser único.
    $ src
    ( cadena ) (Opcional) URL completa de la hoja de estilo, o ruta de la hoja de estilo en relación con el directorio raíz de WordPress.
    
    Valor por defecto: ''
    
    $ deps
    ( matriz ) (Opcional) Una matriz de hoja de estilo registrada maneja esta hoja de estilo depende de.
    
    Valor predeterminado: array ()
    
    $ ver
    ( string | bool | null ) (Opcional) Cadena que especifica el número de versión de la hoja de estilo, si tiene una, que se agrega a la URL como una cadena de consulta para fines de captura de caché. Si la versión se configura como falsa, se agrega automáticamente un número de versión igual a la versión actual instalada de WordPress. Si se establece en nulo, no se agrega ninguna versión.
    
    Valor predeterminado: falso
    
    $ media
    ( cadena ) (Opcional) El medio para el que se ha definido esta hoja de estilo. Acepta tipos de medios como 'todos', 'imprimir' y 'pantalla', o consultas de medios como '(orientación: vertical)' y '(ancho máximo: 640px)'.
    
    Valor predeterminado: 'todos'

--- wp-content/themes/TemaAsesPV/functions.php
+++ wp-content/themes/TemaAsesPV/functions.php
@@ -3,5 +3,7 @@
  *  Carga los script y los CSS del tema
  */
 function asepv_scripts (){
+	/*Styles*/
+	wp_enqueue_style(bootstrap_css,get_template_directory_uri().'/assets/bootstrap/css/bootstrap.min.css',false,false,);
 
 }
