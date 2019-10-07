6ae204805645934d98df5f572e9bb307120e3697
errob <errobimd@gmail.com>
2019-10-07 22:21:23 +0200

    Se pone el codigo de la cabecera y se añade wp_head() para insertar los estilos

--- wp-content/themes/TemaAsesPV/header.php
+++ wp-content/themes/TemaAsesPV/header.php
@@ -1,1 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>asespb</title>
+    <link rel="icon" type="image/png" sizes="undefinedxundefined" href="assets/img/aseslogo.png?h=8ec2db803c9ffdbe46435e40ca668d71">
+    <link rel="icon" type="image/png" sizes="undefinedxundefined" href="assets/img/ases.png?h=6ee3b480ae4a57cd8f63259d1354ef06">
+	<?php wp_head();?>
+    <!--<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css?h=462dc43c75af8bcc6981d7e1aa258be6">-->
+    <link rel="manifest" href="manifest.json?h=d9416c47a5d4c0093688b57d33f5d6dd">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+<!--    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css?h=cfa788754642173dafbd830bd7969fdb">
+    <link rel="stylesheet" href="assets/css/Article-Clean.css?h=5d2455e1fa7653ce290cec1c4efa33f9">
+    <link rel="stylesheet" href="assets/css/Article-Dual-Column.css?h=bd7cecc64d89771757200462df6f9655">
+    <link rel="stylesheet" href="assets/css/Article-List.css?h=cec575f25bba55cfaa10100750e76412">
+    <link rel="stylesheet" href="assets/css/Projects-Clean.css?h=a788dfb0658dfe6130c264c3c32d77f0">
+    <link rel="stylesheet" href="assets/css/styles.css?h=e0a0a353e4c8c860137115468f9ed6d0">-->
+</head>
+
+<body style="background-color: rgb(41,165,216);">
+    <!-- Start: Cabecera -->
+    <section class="container">
+        <div class="row">
+            <div class="col text-center"><img class="img-fluid" src="assets/img/ases.png?h=6ee3b480ae4a57cd8f63259d1354ef06"></div>
+        </div>
+    </section>
+    <!-- End: Cabecera -->
