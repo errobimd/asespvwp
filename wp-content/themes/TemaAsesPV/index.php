9bf03100fc7cc082b8245ac041596d4af457197e
errob <errobimd@gmail.com>
2019-10-13 23:14:55 +0200

    Despues de los ajustes en el panel de administracion de wordpress en el que en ajustes/lectura ponemos como paginas estaticas. Despues elejimos la pagina inicio como portada y la pagina de entradas como blog. Ponemos para probas el texto Hola

--- wp-content/themes/TemaAsesPV/index.php
+++ wp-content/themes/TemaAsesPV/index.php
@@ -1,6 +1,8 @@
 <?php
 /*echo "Aqui va el header";*/
-	get_header();
-/*echo "Aqui va el footer";*/
-	get_footer();
+	get_header(); ?>
+<!--Ponemos hola para ver como se ver la pagina blog-->
+	<h1>Hola</h1>
+<?php/*echo "Aqui va el footer";*/
+	get_footer(); ?>
 	
