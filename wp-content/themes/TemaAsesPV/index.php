29baf396df2c0f7fd83b0e71b24d8468ec74b3ca
errob <errobimd@gmail.com>
2019-10-17 19:00:12 +0200

    Ponemos en titulo del articulo dependiendo del post actual

--- wp-content/themes/TemaAsesPV/index.php
+++ wp-content/themes/TemaAsesPV/index.php
@@ -16,7 +16,11 @@
                 para que sea responsibe-->
                 <!--<img class="img-fluid" src="assets/img/building.jpg">-->
                 <?php the_post_thumbnail('mediano',array('class'=>'img-fluid'));?>
-                <h3 class="name"><strong>Título Artículo</strong></h3>
+
+                <!--Ponemos en titulo del articulo dependiendo del post actual-->
+                <!--<h3 class="name"><strong>Título Artículo</strong></h3>-->
+                <h3 class="name"><strong><?php the_title();?></strong></h3>
+
                 <div style="font-size: 11px;"><span style="font-size: 10px;">Artículo creado el : 6 Octubre 2019&nbsp;</span></div>
                 <div><span style="font-size: 10px;">Autor : Admin&nbsp;</span></div>
                 <p class="border rounded border-info shadow description" style="margin-bottom: 15px;">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p>
