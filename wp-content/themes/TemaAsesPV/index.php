c365da6827d41fbafd6d46b4567b817b65f4114c
errob <errobimd@gmail.com>
2019-10-17 19:14:25 +0200

    Añadimos a la clase imagen las clases border-info y shadow para que le de sombra a la imagen

--- wp-content/themes/TemaAsesPV/index.php
+++ wp-content/themes/TemaAsesPV/index.php
@@ -18,11 +18,7 @@
                 <!--Definimos el tamaño de la imagen y le asignamos la clases img-fluid
                 para que sea responsibe-->
                 <!--<img class="img-fluid" src="assets/img/building.jpg">-->
-                <?php the_post_thumbnail('pequeño',array('class'=>'img-fluid'));?>
-
-                <!--Ponemos en titulo del articulo dependiendo del post actual-->
-                <!--<h3 class="name"><strong>Título Artículo</strong></h3>-->
-                <h3 class="name"><strong><?php the_title();?></strong></h3>
+                <?php the_post_thumbnail('pequeño',array('class'=>'img-fluid border-info shadow'));?>
 
                 <div style="font-size: 11px;"><span style="font-size: 10px;">Artículo creado el : 6 Octubre 2019&nbsp;</span></div>
                 <div><span style="font-size: 10px;">Autor : Admin&nbsp;</span></div>
