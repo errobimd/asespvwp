b880089ea6e57d021453fb43146dc777046d316c
errob <errobimd@gmail.com>
2019-10-20 00:34:28 +0200

    Añadir el html del index.html

--- wp-content/themes/TemaAsesPV/front-page.php
+++ wp-content/themes/TemaAsesPV/front-page.php
@@ -1,2 +1,78 @@
 <?php get_header();?>
-
+    <!-- Start: Noticias y Eventos -->
+    <section class="container">
+        <div class="row">
+            <!-- Start: Noticias -->
+            <div class="col" style="margin-bottom: 0.5rem;">
+                <h1 class="text-center" style="font-size: 3vw;">Noticias</h1>
+                <div class="card-group">
+                    <div class="card border rounded" style="margin-right: 0.5rem;"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
+                        <div class="card-body text-center" style="background-color: #c9e8dd;">
+                            <h4 class="card-title" style="font-size: 2vw;">Noticia 1</h4>
+                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">Saber Más</button></div>
+                    </div>
+                    <!-- Start: Se oculta en pantallas pequeñas -->
+                    <div class="card border rounded d-sm-none d-md-block"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
+                        <div class="card-body text-center" style="background-color: #c9e8dd;">
+                            <h4 class="card-title" style="font-size: 2vw;">Noticia 2</h4>
+                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">Saber Más</button></div>
+                    </div>
+                    <!-- End: Se oculta en pantallas pequeñas -->
+                </div>
+            </div>
+            <!-- End: Noticias -->
+            <!-- Start: Eventos -->
+            <div class="col" style="margin-bottom: 0.5rem;">
+                <h1 class="text-center" style="font-size: 3vw;">Eventos</h1>
+                <div class="card-group border-warning">
+                    <div class="card border rounded" style="margin-right: 0.5rem;"><img class="img-fluid card-img-top w-100 d-block border rounded" src="http://lorempixel.com/400/200/">
+                        <div class="card-body text-center" style="background-color: #c9e8dd;">
+                            <h4 class="card-title" style="font-size: 2vw;">Evento 1</h4>
+                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">¿Cuando es?</button></div>
+                    </div>
+                    <!-- Start: Se oculta en pantallas pequeñas -->
+                    <div class="card d-sm-none d-md-block"><img class="rounded img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
+                        <div class="card-body text-center" style="background-color: #c9e8dd;">
+                            <h4 class="card-title" style="font-size: 2vw;">Evento 2</h4>
+                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">¿Cuando es?</button></div>
+                    </div>
+                    <!-- End: Se oculta en pantallas pequeñas -->
+                </div>
+            </div>
+            <!-- End: Eventos -->
+        </div>
+    </section>
+    <!-- End: Noticias y Eventos -->
+    <!-- Start: Artículos del blog -->
+    <section class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center" style="font-size: 3vw;">Artículos</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col" style="margin-bottom: 0.5rem;">
+                <div class="card-group">
+                    <div class="card border rounded" style="margin-right: 0.5rem;"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
+                        <div class="card-body text-center" style="background-color: #c9e8dd;">
+                            <h4 class="card-title" style="font-size: 2vw;">Articulo 1</h4>
+                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">Saber más</button></div>
+                    </div>
+                    <div class="card border rounded" style="margin-right: 0.5rem;"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
+                        <div class="card-body text-center" style="background-color: #c9e8dd;">
+                            <h4 class="card-title" style="font-size: 2vw;">Articulo 2</h4>
+                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">Saber más</button></div>
+                    </div>
+                    <!-- Start: Se oculta en pantallas pequeñas -->
+                    <div class="card border rounded d-sm-none d-md-block"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
+                        <div class="card-body text-center" style="font-size: 1vw;background-color: #c9e8dd;">
+                            <h4 class="card-title" style="font-size: 2vw;">Articulo 3</h4>
+                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button">Saber más</button></div>
+                    </div>
+                    <!-- End: Se oculta en pantallas pequeñas -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End: Artículos del blog -->
+<?php get_footer();?>
