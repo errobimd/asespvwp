3de27a93fea2229704268f72988740c009733b3d
errob <errobimd@gmail.com>
2019-10-21 01:11:45 +0200

    Movemos el codigo del front-page.php a queries.php

--- wp-content/themes/TemaAsesPV/assets/inc/queries.php
+++ wp-content/themes/TemaAsesPV/assets/inc/queries.php
@@ -9,7 +9,11 @@ function ases_query_noticias ($cantidad=-1){
 	printf('<pre>%s</pre>',var_export(get_post_custom(get_the_ID()),true))
 
 	?>
-
+		<div class="card border rounded" style="margin-right: 0.5rem;"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
+			<div class="card-body text-center" style="background-color: #c9e8dd;">
+				<h4 class="card-title" style="font-size: 2vw;">Noticia 1</h4>
+				<p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">Saber Más</button></div>
+		</div>
 
 
 	<?php
