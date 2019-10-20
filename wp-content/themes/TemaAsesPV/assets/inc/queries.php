88cf44fe32c72786e32d5a1558ce1cb05190dbe7
errob <errobimd@gmail.com>
2019-10-21 00:45:08 +0200

    Hacemos la consulta para despues reflejarla en el codigo html

--- wp-content/themes/TemaAsesPV/assets/inc/queries.php
+++ wp-content/themes/TemaAsesPV/assets/inc/queries.php
@@ -3,6 +3,16 @@ function ases_query_noticias ($cantidad=-1){
 	$args=array(
 		'post_type'=> 'noticias',
 		'post_per_page'=>$cantidad // -1 son todas la noticias.
-
 	);
+	$noticias = new WP_Query($args);
+	while ($noticias->have_posts()):$noticias->the_post();
+
+
+	?>
+
+
+
+	<?php
+	endwhile;wp_reset_postdata();
+
 }
