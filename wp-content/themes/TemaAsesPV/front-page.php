17775caf1048a745c8fe09425d379d845fa729a5
errob <errobimd@gmail.com>
2019-10-23 20:38:58 +0200

    Quitar el codigo estatico y dejar la funcion (ases_query_noticias())  para que muestre el contenido dinamico.

--- wp-content/themes/TemaAsesPV/front-page.php
+++ wp-content/themes/TemaAsesPV/front-page.php
@@ -3,7 +3,7 @@
     <section class="container">
         <div class="row">
             <!-- Start: Noticias -->
-	                <?php ases_query_noticias(1)?>
+	                <?php ases_query_noticias(2)?>
             <!-- End: Noticias -->
             <!-- Start: Eventos -->
 			        <?php ases_query_eventos(2)?>
@@ -14,33 +14,7 @@
     <!-- Start: Artículos del blog -->
     <section class="container">
         <div class="row">
-            <div class="col">
-                <h1 class="text-center" style="font-size: 3vw;">Artículos</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col" style="margin-bottom: 0.5rem;">
-                <?php ases_query_articulos(1)?>
-                <div class="card-group">
-                    <div class="card border rounded" style="margin-right: 0.5rem;"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
-                        <div class="card-body text-center" style="background-color: #c9e8dd;">
-                            <h4 class="card-title" style="font-size: 2vw;">Articulo 1</h4>
-                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">Saber más</button></div>
-                    </div>
-                    <div class="card border rounded" style="margin-right: 0.5rem;"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
-                        <div class="card-body text-center" style="background-color: #c9e8dd;">
-                            <h4 class="card-title" style="font-size: 2vw;">Articulo 2</h4>
-                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="font-size: 1vw;">Saber más</button></div>
-                    </div>
-                    <!-- Start: Se oculta en pantallas pequeñas -->
-                    <div class="card border rounded d-sm-none d-md-block"><img class="img-fluid card-img-top w-100 d-block" src="http://lorempixel.com/400/200/">
-                        <div class="card-body text-center" style="font-size: 1vw;background-color: #c9e8dd;">
-                            <h4 class="card-title" style="font-size: 2vw;">Articulo 3</h4>
-                            <p class="text-justify card-text" style="font-size: 1vw;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button">Saber más</button></div>
-                    </div>
-                    <!-- End: Se oculta en pantallas pequeñas -->
-                </div>
-            </div>
+                <?php ases_query_articulos(3)?>
         </div>
     </section>
     <!-- End: Artículos del blog -->
